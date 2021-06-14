abcb079119509d13a766000cf600a9434f9ccaa0
justgoodin <rehan.haider.v3@gmail.com>
2021-06-14 19:14:45 +0000

    Solution

--- model/taskman.py
+++ model/taskman.py
@@ -6,7 +6,7 @@ from typing import List, Optional
 filepath = "data/tasks.json"
 
 
-def data_to_json(data: List):
+async def data_to_json(data: List):
     """TODO
     1. Take input data, of a list of tasks
     2. Write the data into a json file (tasks.json)
@@ -16,7 +16,7 @@ def data_to_json(data: List):
         file.write(data)
 
 
-def get_tasks(id: Optional[int] = 0):
+async def get_tasks(id: Optional[int] = 0):
     """TODO
     1. Fetch all tasks if no argument (id) provided
     2. Else fetch the task by id provided
@@ -27,7 +27,7 @@ def get_tasks(id: Optional[int] = 0):
     return response
 
 
-def create_task(new_task: Task):
+async def create_task(new_task: Task):
     """TODO
     1. Create a new task and add it to the list of tasks
     2. Write the updated tasklist to file
@@ -36,23 +36,23 @@ def create_task(new_task: Task):
     id = max([task.id for task in tasks]) + 1
     tasks.append(TaskList(id=id, task=new_task))
     data = [task.dict() for task in tasks]
-    data_to_json(data)
+    await data_to_json(data)
     return id
 
 
-def delete_task(id):
+async def delete_task(id):
     """TODO
     1. Delete the task by id provided
     """
     tasks = parse_file_as(List[TaskList], "data/tasks.json")
     tasks = [task for task in tasks if task.id != id]
     data = [task.dict() for task in tasks]
-    data_to_json(data)
+    await data_to_json(data)
 
     return id
 
 
-def update_task(id: int, new_task: Task):
+async def update_task(id: int, new_task: Task):
     """TODO
     1. Update the task by id based on new task details
     2. write the updated tasklist to file
@@ -63,6 +63,6 @@ def update_task(id: int, new_task: Task):
         if task["id"] == id:
             task["task"] = new_task.dict()
 
-    data_to_json(data)
+    await data_to_json(data)
 
     return id
