a88ae1faaa8f987292176aeb487248027fe785e5
justgoodin <rehan.haider.v3@gmail.com>
2021-06-14 19:02:43 +0000

    Starter Kit

--- model/taskman.py
+++ model/taskman.py
@@ -6,7 +6,7 @@ from typing import List, Optional
 filepath = "data/tasks.json"
 
 
-def data_to_json(data: List):
+async def data_to_json(data: List):
     """
     TODO
     1. Take input data, of a list of tasks
@@ -15,7 +15,7 @@ def data_to_json(data: List):
     pass
 
 
-def get_tasks(id: Optional[int] = 0):
+async def get_tasks(id: Optional[int] = 0):
     """
     TODO
     1. Fetch all tasks if no argument (id) provided
@@ -24,7 +24,7 @@ def get_tasks(id: Optional[int] = 0):
     return "response"
 
 
-def create_task(new_task: Task):
+async def create_task(new_task: Task):
     """
     TODO:
     1. Create a new task and add it to the list of tasks
@@ -33,7 +33,7 @@ def create_task(new_task: Task):
     return "id"
 
 
-def delete_task(id):
+async def delete_task(id):
     """
     TODO:
     1. Delete the task by id provided
@@ -41,7 +41,7 @@ def delete_task(id):
     return "id"
 
 
-def update_task(id: int, new_task: Task):
+async def update_task(id: int, new_task: Task):
     """
     TODO
     1. Update the task by id based on new task details
